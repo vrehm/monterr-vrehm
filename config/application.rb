912f3c8dd52ce35c3c35637233e6f6f145caf756
Vincent REHM <contact@vincentrehm.fr>
2022-10-12 17:30:04 +0200

    cleaning

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,5 @@ module App
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_dispatch.rescue_responses.merge!(
-      { "ActiveRecord::RecordNotFound"   => :not_found, 
-        "ActiveRecord::StaleObjectError" => :conflict,
-        "ActiveRecord::RecordInvalid"    => :unprocessable_entity,
-        "ActiveRecord::RecordNotSaved"   => :unprocessable_entity }
-    )
   end
 end
