4976e547d6cc1c883e34ed2f3227503907453227
Vincent REHM <contact@vincentrehm.fr>
2022-10-12 17:28:37 +0200

    typos

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -1,7 +1,7 @@
 class Commune < ApplicationRecord
   belongs_to :intercommunality
-  has_many :commune_street
-  has_many :streets, through: :commune_street
+  has_many :commune_streets
+  has_many :streets, through: :commune_streets
   validates :name, :code_insee, presence: true
   validates :code_insee, format: {with: /\A\d{5}\z/}
 
--- app/models/street.rb
+++ app/models/street.rb
@@ -1,6 +1,6 @@
 class Street < ApplicationRecord
-  has_many :commune_street
-  has_many :communes, through: :commune_street
+  has_many :commune_streets
+  has_many :communes, through: :commune_streets
   validates :title, presence: true
   validates :from, :to, numericality: {only_integer: true, greater_than: 0}, allow_nil: true
   validate :to_is_greater_than_from
