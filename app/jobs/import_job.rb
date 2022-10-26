e4e9c05f218dcee260741aeba8433206c7fa9777
Vincent REHM <contact@vincentrehm.fr>
2022-10-26 18:29:49 +0200

    refactoring import job with performance mindset

--- app/jobs/import_job.rb
+++ app/jobs/import_job.rb
@@ -5,41 +5,47 @@ class ImportJob < ApplicationJob
 
   def perform(csv)
     intercommunalities, communes = {}, {}
+    
     CSV.foreach(csv, headers: true, col_sep: ";", encoding: "ISO-8859-1", header_converters: :symbol) do |row|
-      intercommunalities[row[:siren_epci]] ||= create_intercommunality({
+    # CSV.foreach(csv, headers: true, col_sep: ",", encoding: "UTF-8", header_converters: :symbol) do |row|
+      intercommunalities[row[:siren_epci]] ||= {
         siren: row[:siren_epci],
         name: row[:nom_complet],
-        form: row[:form_epci]
-      })
-      communes[row[:insee]] ||= create_commune({
+        form: row[:form_epci].downcase.gsub("metro", "met"),
+        slug: row[:nom_complet].parameterize,
+        created_at: Time.now,
+        updated_at: Time.now,
+        population: 0
+      }
+
+      communes[row[:insee]] ||= {
         code_insee: row[:insee],
         name: row[:nom_com],
         population: row[:pop_total],
-        intercommunality: row[:siren_epci]
-      })
+        intercommunality_id: nil,
+        created_at: Time.now,
+        updated_at: Time.now,
+        siren_epci: row[:siren_epci] 
+      }
     end
+
+    Intercommunality.upsert_all(intercommunalities.values, unique_by: :siren)
+    inter_db = Intercommunality.all.pluck(:siren, :id).to_h
+    
+    communes.values.each do |commune|
+      commune[:intercommunality_id] = inter_db[commune[:siren_epci]]
+      commune.delete(:siren_epci)
+    end
+    Commune.upsert_all(communes.values, unique_by: :code_insee)
+
     intercommunalities.each do |siren, intercommunality|
-      intercommunality = Intercommunality.find_by_siren(intercommunality[:siren])
-      intercommunality.update!(population: intercommunality.communes.sum(:population))
+      id = inter_db[siren]
+      population = Intercommunality.where(id: id).includes(:communes).sum("communes.population")
+      intercommunality[:population] = population
+      intercommunality[:updated_at] = Time.now
     end
+
+    Intercommunality.upsert_all(intercommunalities.values, unique_by: :siren)
     puts "#{intercommunalities.size} intercommunes, #{communes.size} communes"
-  end
-
-  private
-
-  def create_intercommunality(raw_hash)
-    raw_hash[:form] = raw_hash[:form].downcase.gsub("metro", "met")
-    raw_hash[:slug] = raw_hash[:name].parameterize
-    Intercommunality.create!(raw_hash) unless Intercommunality.find_by_siren(raw_hash[:siren]).present?
-    raw_hash
-  end
-
-  def create_commune(raw_hash)
-    intercommunality = Intercommunality.find_by_siren(raw_hash[:intercommunality])
-    return if intercommunality.nil?
-    raw_hash.delete(:intercommunality)
-    raw_hash[:intercommunality_id] = intercommunality.id
-    Commune.create!(raw_hash) unless Commune.find_by_code_insee(raw_hash[:code_insee]).present?
-    raw_hash
   end
 end
--- app/models/intercommunality.rb
+++ app/models/intercommunality.rb
@@ -2,7 +2,7 @@ class Intercommunality < ApplicationRecord
   has_many :communes
   validates :name, :siren, presence: true
   validates :siren, uniqueness: {case_sensitive: false}, format: {with: /\A\d{9}\z/}
-  validates :form, inclusion: {in: %w[ca cu cc met]}
+  validates :form, inclusion: {in: %w[ca cu cc met me]}
   before_save :generate_slug
 
   def communes_hash
